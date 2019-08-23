8bb6615518115af18ba63f5f1d65493d8cf37da1
Pete Jemian <prjemian@gmail.com>
2019-08-23 17:11:07 -0500

    WIP #31

--- profile_bluesky/startup/35-DM-support.py
+++ profile_bluesky/startup/35-DM-support.py
@@ -3,3 +3,10 @@ logger.info(__file__)
 """
 support for APS data management
 """
+
+
+class EpicsSignalDM(EpicsSignal):
+    """custom class for Data Management metadata"""
+    label = None
+    h5address = None
+

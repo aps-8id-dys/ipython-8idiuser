0f3e10368c67c898c6398b88613530782e0c25ac
Pete Jemian <prjemian@gmail.com>
2019-08-28 16:46:53 -0500

    MNT #34 refactor

--- profile_bluesky/startup/records/epidRecord.py
+++ profile_bluesky/startup/records/epidRecord.py
@@ -24,32 +24,39 @@ Public Structures
 
 from ophyd.device import Device, Component
 from ophyd import EpicsSignal, EpicsSignalRO
+from .common_fields import EpicsRecordDeviceCommonAll
+from .common_fields import EpicsRecordFloatFields
 
 
 __all__ = ["epidRecord", ]
 
 
-class epidRecord(Device):
+class epidRecord(EpicsRecordFloatFields, EpicsRecordDeviceCommonAll):
+    """
+    EPICS epid record support in ophyd
+    
+    :see: https://epics.anl.gov/bcda/synApps/std/epidRecord.html
+    """
     proportional_gain = Component(EpicsSignal, ".KP")
     integral_gain = Component(EpicsSignal, ".KI")
     derivative_gain = Component(EpicsSignal, ".KD")
-    following_errror = Component(EpicsSignalRO, ".ERR")
+
+    following_error = Component(EpicsSignalRO, ".ERR")
+    output_value = Component(EpicsSignalRO, ".OVAL")
+
     calculated_P = Component(EpicsSignalRO, ".P")
     calculated_I = Component(EpicsSignal, ".I")
     calculated_D = Component(EpicsSignalRO, ".D")
-    output_value = Component(EpicsSignalRO, ".OVAL")
+
     clock_ticks = Component(EpicsSignalRO, ".CT")
     time_difference = Component(EpicsSignal, ".DT")
+
+    # limits imposed by the record support:
+    #     .LOPR <= .OVAL <= .HOPR
+    #     .LOPR <= .I <= .HOPR
     high_limit = Component(EpicsSignal, ".DRVH")
     low_limit = Component(EpicsSignal, ".DRVL")
-    
-    # TODO: additional fields common to all records (such as alarms)
-    description = Component(EpicsSignal, ".DESC")
 
-    # TODO: additional fields common to similar records (such as operating range)
-    units = Component(EpicsSignal, ".EGU")
-    precision = Component(EpicsSignal, ".PREC")
-    
     @property
     def value(self):
         return self.output_value.value

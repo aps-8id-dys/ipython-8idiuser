bf9a11b100b85078d403f9b469dfe91b70e056fc
Pete Jemian <prjemian@gmail.com>
2019-09-06 09:35:49 -0500

    ENH #53 add stage_setup method

--- profile_bluesky/startup/25-areadetector.py
+++ profile_bluesky/startup/25-areadetector.py
@@ -116,6 +116,29 @@ class Lambda750kAreaDetector(SingleTrigger, AreaDetector):
     immout = Component(IMMFilePlugin, "IMMout:")
     gather = Component(GatherPlugin, "Gather1:")
     scatter = Component(ScatterPlugin, "Scatter1:")
+    
+    def staging_setup_DM(self, file_path, file_name,
+            num_images, acquire_time, acquire_period):
+        """
+        setup the detector's stage_sigs for acquisition with the DM workflow
+        
+        Implement this method in _any_ Device that requires custom
+        setup for the DM workflow.
+        
+        Note: might need to generalize the argument list here
+        """
+        self.cam.stage_sigs["num_images"] = num_images
+        self.cam.stage_sigs["acquire_time"] = acquire_time
+        self.cam.stage_sigs["acquire_period"] = acquire_period
+        self.immout.stage_sigs["enable"] = 1
+        self.immout.stage_sigs["blocking_callbacks"] = "Yes"
+        self.immout.stage_sigs["parent.cam.array_callbacks"] = 1
+        self.immout.stage_sigs["file_path"] = file_path
+        self.immout.stage_sigs["file_name"] = file_name
+        self.immout.stage_sigs["num_capture"] = num_images
+        self.immout.stage_sigs["file_number"] = 1
+        self.immout.stage_sigs["file_format"] = "IMM_Cmprs"
+        self.immout.stage_sigs["capture"] = 1
 
 
 try:

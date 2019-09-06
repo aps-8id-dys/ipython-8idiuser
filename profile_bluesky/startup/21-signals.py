dcb0733ac59fc45eed76645aebd1d1350e2a6531
qzhang234 <48140482+qzhang234@users.noreply.github.com>
2019-09-06 15:20:32 -0500

    ENH #55: Added slit objects

--- profile_bluesky/startup/21-signals.py
+++ profile_bluesky/startup/21-signals.py
@@ -20,6 +20,12 @@ shutter.delay_s = 0.05 # shutter needs short recovery time after moving
 crl = CompoundRefractiveLensDevice(name="crl")
 monochromator = MonochromatorDevice(name="monochromator")
 sa1 = SlitA1Device(name="sa1")
+si1 = SlitI1Device(name="si1")
+si2 = SlitI2Device(name="si2")
+si3 = SlitI3Device(name="si3")
+si4 = SlitI4Device(name="si4")
+si5 = SlitI5Device(name="si5")
+sipink = SlitIpinkDevice(name="sipink")
 
 
 class ModifiedEpidRecord(EpidRecord):

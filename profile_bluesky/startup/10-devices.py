7260530949315014602f8ef91b65c8ab27e6d818
qzhang234 <48140482+qzhang234@users.noreply.github.com>
2019-09-06 14:36:56 -0500

    ENH #55: Create SlitA1Device

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -17,3 +17,18 @@ class MonochromatorDevice(Device):
     piezo = Component(EpicsMotor, '8idimono:m4', labels=["motor", "mono", "optics"])
     monopic = Component(EpicsMotor, '8idimono:m1', labels=["motor", "mono", "optics"])
     nano = Component(EpicsMotor, '8idimono:m5', labels=["motor", "mono", "optics"])
+
+class SlitA1Device(Device):  
+    """
+    Slit1 in 8-ID-A
+    """
+    vgap = Component(EpicsMotor, '8ida:Slit1Vsize', labels=["motor", "slit"])
+    vcen = Component(EpicsMotor, '8ida:Slit1Vcenter', labels=["motor", "slit"])
+    hgap = Component(EpicsMotor, '8ida:Slit1Hsize', labels=["motor", "slit"])
+    hcen = Component(EpicsMotor, '8ida:Slit1Hcenter', labels=["motor", "slit"])
+    zu = Component(EpicsMotor, '8ida:m11', labels=["motor", "slit"])    
+    xu = Component(EpicsMotor, '8ida:m14', labels=["motor", "slit"])    
+    zd = Component(EpicsMotor, '8ida:m15', labels=["motor", "slit"])
+    xd = Component(EpicsMotor, '8ida:m16', labels=["motor", "slit"])
+
+    

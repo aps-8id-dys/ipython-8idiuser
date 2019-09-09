9f3418fafaad62f37eefc125e3f5ecdee8ecd408
qzhang234 <48140482+qzhang234@users.noreply.github.com>
2019-09-09 00:27:44 -0500

    ENH #56: Merged motors sharing same stage
    Moved motor definitions from startup 15-spec-config into 10-devices and merged all that share the same stage under the same class

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -18,6 +18,7 @@ class MonochromatorDevice(Device):
     pico = Component(EpicsMotor, '8idimono:m1', labels=["motor", "mono", "optics"])
     nano = Component(EpicsMotor, '8idimono:m5', labels=["motor", "mono", "optics"])
 
+    
 class SlitA1Device(Device):  
     """
     Slit1 in 8-ID-A
@@ -31,6 +32,7 @@ class SlitA1Device(Device):
     zd = Component(EpicsMotor, '8ida:m15', labels=["motor", "slit"])
     xd = Component(EpicsMotor, '8ida:m16', labels=["motor", "slit"])
 
+    
 class SlitI1Device(Device):  
     """
     Slit1 in 8-ID-I
@@ -40,6 +42,7 @@ class SlitI1Device(Device):
     vcen = Component(EpicsMotor, '8idi:Slit1Vcenter', labels=["motor", "slit"])
     hgap = Component(EpicsMotor, '8idi:Slit1Hsize', labels=["motor", "slit"])
     hcen = Component(EpicsMotor, '8idi:Slit1Hcenter', labels=["motor", "slit"])
+
     
 class SlitI2Device(Device):  
     """
@@ -49,7 +52,8 @@ class SlitI2Device(Device):
     vcen = Component(EpicsMotor, '8idi:Slit2Vcenter', labels=["motor", "slit"])
     hgap = Component(EpicsMotor, '8idi:Slit2Hsize', labels=["motor", "slit"])
     hcen = Component(EpicsMotor, '8idi:Slit2Hcenter', labels=["motor", "slit"])    
-    
+  
+
 class SlitI3Device(Device):  
     """
     Slit3 in 8-ID-I
@@ -58,7 +62,8 @@ class SlitI3Device(Device):
     vcen = Component(EpicsMotor, '8idi:Slit3Vcenter', labels=["motor", "slit"])
     hgap = Component(EpicsMotor, '8idi:Slit3Hsize', labels=["motor", "slit"])
     hcen = Component(EpicsMotor, '8idi:Slit3Hcenter', labels=["motor", "slit"])    
-    
+ 
+
 class SlitI4Device(Device):  
     """
     Slit4 in 8-ID-I
@@ -67,7 +72,8 @@ class SlitI4Device(Device):
     vcen = Component(EpicsMotor, '8idi:Slit4Vcenter', labels=["motor", "slit"])
     hgap = Component(EpicsMotor, '8idi:Slit4Hsize', labels=["motor", "slit"])
     hcen = Component(EpicsMotor, '8idi:Slit4Hcenter', labels=["motor", "slit"])    
-    
+ 
+
 class SlitI5Device(Device):  
     """
     Slit5 in 8-ID-I
@@ -79,6 +85,7 @@ class SlitI5Device(Device):
     hgap = Component(EpicsMotor, '8idi:Slit5Hsize', labels=["motor", "slit"])
     hcen = Component(EpicsMotor, '8idi:Slit5Hcenter', labels=["motor", "slit"])
 
+    
 class SlitIpinkDevice(Device):  
     """
     Slitpink in 8-ID-I
@@ -89,4 +96,109 @@ class SlitIpinkDevice(Device):
     hcen = Component(EpicsMotor, '8idi:SlitpinkHcenter', labels=["motor", "slit"])
     
     
-   
+class TA1(Device):  
+    """
+    Optics Table 1 in 8-ID-A
+    """    
+    x = Component(EpicsMotor, '8ida:TA1:x', labels=["motor", "table"])  # Can I make up label called 'table'?
+    z = Component(EpicsMotor, '8ida:TA1:y', labels=["motor", "table"])
+  
+
+class TA2(Device):  
+    """
+    Optics Table 2 in 8-ID-A
+    """    
+    x = Component(EpicsMotor, '8ida:TA2:x', labels=["motor", "table"])
+    z = Component(EpicsMotor, '8ida:TA2:y', labels=["motor", "table"])
+    fine = Component(EpicsMotor, '8ida:sm9', labels=["motor", "table"])
+
+
+class DiamondMirror(Device):  
+    """
+    I/E Beam-splitting Mirror in 8-ID-D
+    """    
+    x = Component(EpicsMotor, '8idd:m1', labels=["motor", "optics"])
+    z = Component(EpicsMotor, '8idd:m2', labels=["motor", "optics"])
+
+
+class TI1(Device):  
+    """
+    Optics Table 1 in 8-ID-I
+    """    
+    x = Component(EpicsMotor, '8idi:TI1:x', labels=["motor", "table"])
+    z = Component(EpicsMotor, '8idi:TI1:y', labels=["motor", "table"])
+    
+    
+class TI2(Device):  
+    """
+    Optics Table 2 in 8-ID-I
+    """    
+    x = Component(EpicsMotor, '8idi:TI2:x', labels=["motor", "table"])
+    z = Component(EpicsMotor, '8idi:TI2:y', labels=["motor", "table"])
+    
+    
+class TI3(Device):  
+    """
+    Optics Table 3 in 8-ID-I
+    """    
+    x = Component(EpicsMotor, '8idi:TI3:x', labels=["motor", "table"])
+    y = Component(EpicsMotor, '8idi:m16', labels=["motor", "table"])  # ? Why is ti3 the only one that has a y stage?
+    z = Component(EpicsMotor, '8idi:TI3:y', labels=["motor", "table"])
+    
+
+class TI4(Device):  
+    """
+    Optics Table 4 in 8-ID-I
+    """    
+    x = Component(EpicsMotor, '8idi:TI4:x', labels=["motor", "table"])
+    z = Component(EpicsMotor, '8idi:TI4:y', labels=["motor", "table"])
+    
+    
+class BeWindow(Device):  
+    """
+    Beryllium Window 8-ID-I
+    """    
+    x = Component(EpicsMotor, '8idi:m17', labels=["motor", "optics"])
+    z = Component(EpicsMotor, '8idi:m11', labels=["motor", "optics"])
+
+        
+class Shutter(Device):  
+    """
+    Shutter Stage at 8-ID-I
+    """    
+    x = Component(EpicsMotor, '8idi:m1', labels=["motor", "shutter"])
+    z = Component(EpicsMotor, '8idi:m2', labels=["motor", "shutter"])
+ 
+        
+class Shutter(Device):  
+    """
+    Shutter Stage at 8-ID-I
+    """    
+    x = Component(EpicsMotor, '8idi:m1', labels=["motor", "shutter"])
+    z = Component(EpicsMotor, '8idi:m2', labels=["motor", "shutter"])
+    
+            
+class DetStageU(Device):  
+    """
+    Upstream detector stage at 4 m (the old 'ccdx' and 'ccdz')
+    """    
+    x = Component(EpicsMotor, '8idi:m90', labels=["motor", "det"])
+    z = Component(EpicsMotor, '8idi:m91', labels=["motor", "det"])
+  
+
+class DetStageD(Device):  
+    """
+    Downstream detector stage at 8 m (the old 'fccdx' and 'fccdz')
+    """    
+    x = Component(EpicsMotor, '8idi:m25', labels=["motor", "det"])
+    z = Component(EpicsMotor, '8idi:m83', labels=["motor", "det"])
+
+    
+class Piezo(Device):  
+    """
+    Piezo stage at the sample?
+    """    
+    x = Component(EpicsMotor, '8idi:m69', labels=["motor", "sample"])
+    z = Component(EpicsMotor, '8idi:m70', labels=["motor", "sample"])
+
+    

c4063ba86751617a801d8c6485b5c4cdc8225400
Pete Jemian <prjemian@gmail.com>
2019-09-09 21:10:47 -0500

    MNT #33 adjustments

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -39,13 +39,9 @@ pind4 = scaler1.channels.chan05.s
 pdbs = scaler1.channels.chan06.s
 I_APS = scaler1.channels.chan07.s
 I0Mon = scaler1.channels.chan08.s
+# APD = scaler1.channels.chan09.s
 
 Atten1 = EpicsSignalRO('8idi:userTran1.P', name='Atten1', labels=["detectors",])
 Atten2 = EpicsSignalRO('8idi:userTran3.P', name='Atten2', labels=["detectors",])
 att1 = EpicsSignal('8idi:BioDecode1.A', name='att1', write_pv='8idi:BioEncode1.A')
 att2 = EpicsSignal('8idi:BioDecode2.A', name='att2', write_pv='8idi:BioEncode2.A')
-
-
-T_A = EpicsSignalRO('8idi:LS336:TC4:IN1', name='T_A', labels=["detectors",])
-T_SET = EpicsSignalWithRBV('8idi:LS336:TC4:OUT1:SP', name='T_SET', labels=["detectors",])
-# counter: APD = SpecCounter(mne='APD', config_line='13', name='APD', unit='0', chan='8', pvname=8idi:scaler1.S9)

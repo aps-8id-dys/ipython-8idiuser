60896875cd7d05f4d5e19d294afe786610303f77
S8 I Group Acct <8idiuser@snow.xray.aps.anl.gov>
2019-09-13 17:19:00 -0500

    TST #36 test code - WIP

---
+++ profile_bluesky/startup/spec_support/tester.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+import os
+import sys
+import time
+
+sys.path.append("/home/beams/8IDIUSER/.ipython-bluesky/profile_bluesky/startup")
+
+t0 = time.time()
+from spec_support import spec_DM_support
+connected = f"connected in {time.time()-t0:.3f}s"
+
+print(spec_DM_support.dm_pars.getTable())
+print(connected)
+print(f"finished in {time.time()-t0:.3f}s")
+print(spec_DM_support.dm_workflow.detectors.manufacturerDict)
+
+workflow_file = "/tmp/workflow.h5"
+print(f"before writing HDF5: {workflow_file} exists: {os.path.exists(workflow_file)}")
+spec_DM_support.dm_workflow.create_hdf5_file(workflow_file)
+print(f"after writing HDF5: {workflow_file} exists: {os.path.exists(workflow_file)}")

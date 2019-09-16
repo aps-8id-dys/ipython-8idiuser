f7ec02988c5062d90b9745d1801b8682ee51378f
S8 I Group Acct <8idiuser@snow.xray.aps.anl.gov>
2019-09-15 20:12:37 -0500

    simpler logging setup now

--- profile_bluesky/startup/00-logging.py
+++ profile_bluesky/startup/00-logging.py
@@ -4,18 +4,16 @@ print(__file__)
 configure logging
 """
 
-_log_path = os.path.join(os.getcwd(), ".logs")
-if not os.path.exists(_log_path):
-    os.mkdir(_log_path)
-CONSOLE_IO_FILE = os.path.join(_log_path, ".ipython_console.log")
-
 # start logging console to file
 # https://ipython.org/ipython-doc/3/interactive/magics.html#magic-logstart
 from IPython import get_ipython
 _ipython = get_ipython()
-# %logstart -o -t .ipython_console.log "rotate"
+_log_path = os.path.join(os.getcwd(), ".logs")
+if not os.path.exists(_log_path):
+    os.mkdir(_log_path)
+CONSOLE_IO_FILE = os.path.join(_log_path, ".ipython_console.log")
+del _log_path
 _ipython.magic(f"logstart -o -t {CONSOLE_IO_FILE} rotate")
-
 
 # pip install stdlogpj
 import stdlogpj

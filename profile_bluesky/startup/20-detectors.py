61fa12e24c6579121db6d1086c1fbbd66f6c8e8d
S8 I Group Acct <8idiuser@quartz.xray.aps.anl.gov>
2019-11-04 11:27:22 -0600

    fixes #95

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -45,6 +45,9 @@ I_APS = scaler1.channels.chan07.s
 I0Mon = scaler1.channels.chan08.s
 # APD = scaler1.channels.chan09.s
 
+for item in (pind1, pind2, pind3, pind4, pdbs, I_APS, I0Mon):
+    item._ophyd_labels_ = set(["channel", ])
+
 Atten1 = EpicsSignalRO('8idi:userTran1.P', name='Atten1', labels=["detectors",])
 Atten2 = EpicsSignalRO('8idi:userTran3.P', name='Atten2', labels=["detectors",])
 att1 = EpicsSignal('8idi:BioDecode1.A', name='att1', write_pv='8idi:BioEncode1.A')

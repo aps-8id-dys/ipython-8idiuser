1026d46f3f6bc9eb3f08c6cc2b9f412bad40276c
S8 I Group Acct <8idiuser@quartz.xray.aps.anl.gov>
2020-01-28 17:10:33 -0600

    WIP #127

--- user/lambda_testing.py
+++ user/lambda_testing.py
@@ -10,11 +10,12 @@ test that we can run user ops continuously - use Lambda detector
 
 def lambda_test(num_iter=10):
     for i in range(num_iter):
-        if dm_pars.stop_before_next_scan.value != 0:
+        if dm_pars.stop_before_next_scan.get() != 0:
             logger.info("received signal to STOP before next scan")
             yield from bps.mv(dm_pars.stop_before_next_scan, 0)
             break
 
+        # increment the run number
         yield from bps.mvr(dm_pars.ARun_number, 1)
         file_name = f"A{dm_pars.ARun_number.value:03.0f}"
 
@@ -23,3 +24,36 @@ def lambda_test(num_iter=10):
             num_images=100, file_name=file_name,
             submit_xpcs_job=True,
             atten=None, path=None)
+
+
+def trubble():
+    """
+    demonstrate a problem when using bluesky 1.6.0rc3
+
+    latest release (bluesky 1.4.1) does not raise tihs exception
+
+    see: https://github.com/bluesky/bluesky/issues/1282
+    """
+    import databroker
+    md = {
+        "demonstrate": "trouble",
+        "versions" : {
+            "bluesky": bluesky.__version__,
+            "ophyd": ophyd.__version__,
+            "databroker": databroker.__version__,
+        }
+    }
+    print(md)
+    yield from bps.open_run(md=md)
+    # FIXME: exception here
+    """
+    ProgrammingError: SQLite objects created in a thread can only be used in that same thread. The object was created in thread id 139836100364096 and this is thread id 139834963904256.
+
+    not a problem with bluesky 1.4.1
+    exception with bluesky v1.6.0rc3 and master (2020-01-28)
+    """
+    yield from bps.create('primary')
+    # dm_pars.ARun_number = Component(EpicsSignal, "8idi:Reg173")
+    yield from bps.read(dm_pars.ARun_number)
+    yield from bps.save()
+    yield from bps.close_run()

afc46679463020c946ee9bebbfb53a86efec22f2
S8 I Group Acct <8idiuser@snow.xray.aps.anl.gov>
2020-06-27 17:33:38 -0500

    fixes #172

--- profile_bluesky/startup/instrument/collection.py
+++ profile_bluesky/startup/instrument/collection.py
@@ -19,3 +19,11 @@ from apstools.utils import *
 
 # ensure we return the session logger to the console
 from .session_logs import logger
+
+logger.info(
+    "Disabling log output to the console now."
+    " Log output will still be recorded in log files."
+    " Re-enable with this console-only command:\n"
+    "    logger.addHandler(console_log_writer)"
+    )
+console_log_writer = logger.handlers.pop(0)

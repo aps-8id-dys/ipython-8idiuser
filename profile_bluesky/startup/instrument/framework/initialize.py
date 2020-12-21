f510989f0341dbf79562a638afbc880a982d1a74
S8 I Group Acct <8idiuser@wow.xray.aps.anl.gov>
2020-12-21 13:55:27 -0600

    MNT #246 apply set_defaults()

--- profile_bluesky/startup/instrument/framework/initialize.py
+++ profile_bluesky/startup/instrument/framework/initialize.py
@@ -1,18 +1,23 @@
-
 """
 initialize the bluesky framework
 """
 
 __all__ = [
-    'RE', 'db', 'sd',
-    'bec', 'peaks',
-    'bp', 'bps', 'bpp',
-    'summarize_plan',
-    'np',
-    'callback_db',
-    ]
+    "bec",
+    "bp",
+    "bpp",
+    "bps",
+    "callback_db",
+    "db",
+    "np",
+    "peaks",
+    "RE",
+    "sd",
+    "summarize_plan",
+]
 
 from ..session_logs import logger
+
 logger.info(__file__)
 
 from bluesky import RunEngine
@@ -30,6 +35,7 @@ from ophyd.signal import EpicsSignalBase
 import databroker
 import os
 import sys
+import warnings
 
 
 # convenience imports
@@ -41,13 +47,7 @@ import numpy as np
 
 # add parent directory of instrument package to import path
 sys.path.append(
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            "..",
-        )
-    )
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..",))
 )
 
 
@@ -56,7 +56,7 @@ def get_md_path():
     if os.environ == "win32":
         home = os.environ["LOCALAPPDATA"]
         path = os.path.join(home, md_dir_name)
-    else:       # at least on "linux"
+    else:  # at least on "linux"
         home = os.environ["HOME"]
         path = os.path.join(home, ".config", md_dir_name)
     return path
@@ -67,10 +67,11 @@ old_md = None
 md_path = get_md_path()
 if not os.path.exists(md_path):
     logger.info(
-        "New directory to store RE.md between sessions: %s", 
-        md_path)
+        "New directory to store RE.md between sessions: %s", md_path
+    )
     os.makedirs(md_path)
     from bluesky.utils import get_history
+
     old_md = get_history()
 
 # Set up a RunEngine and use metadata backed PersistentDict
@@ -88,7 +89,7 @@ db = databroker.catalog["mongodb_config"].v1
 
 # Subscribe metadatastore to documents.
 # If this is removed, data is not saved to metadatastore.
-callback_db['db'] = RE.subscribe(db.insert)
+callback_db["db"] = RE.subscribe(db.insert)
 
 # Set up SupplementalData.
 sd = SupplementalData()
@@ -103,19 +104,34 @@ get_ipython().register_magics(BlueskyMagics)
 
 # Set up the BestEffortCallback.
 bec = BestEffortCallback()
-callback_db['bec'] = RE.subscribe(bec)
+callback_db["bec"] = RE.subscribe(bec)
 peaks = bec.peaks  # just as alias for less typing
 bec.disable_baseline()
 
-# At the end of every run, verify that files were saved and
-# print a confirmation message.
-# callback_db['post_run_verify'] = RE.subscribe(post_run(verify_files_saved), 'stop')
+# uncomment to use these diagnostics
 
-# Uncomment to turn on verbose messages for debugging.
+# Verify files saved at end of run, print confirmation message.
+# _prv_ = RE.subscribe(post_run(verify_files_saved), 'stop')
+# callback_db['post_run_verify'] = _prv_
+
+# Verbose debugging messages.
 # ophyd.logger.setLevel(logging.DEBUG)
 
-# diagnostics
-#RE.msg_hook = ts_msg_hook
+# RunEngine Message processing
+# RE.msg_hook = ts_msg_hook
 
-# set default timeout for all EpicsSignalBase connections & communications
-EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=5)
+
+# set default timeout for all EpicsSignal connections & communications
+try:
+    EpicsSignalBase.set_defaults(
+        auto_monitor=True,
+        timeout=60,
+        write_timeout=60,
+        connection_timeout=20,
+    )
+except Exception as exc:
+    warnings.warn(
+        "ophyd version is old, upgrade to 1.6.0+ "
+        "to get set_defaults() method"
+    )
+    EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=5)

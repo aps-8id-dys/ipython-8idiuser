2d679d1d2a4882ebb6fec6d5b2c4985eb89bc5b7
S8 I Group Acct <8idiuser@wow.xray.aps.anl.gov>
2021-02-02 13:05:42 -0600

    WIP #251 after testing cam support with IOC

--- profile_bluesky/startup/instrument/devices/ad_rigaku_detector.py
+++ profile_bluesky/startup/instrument/devices/ad_rigaku_detector.py
@@ -1,4 +1,3 @@
-
 """
 ADRigaku UFXC area detector (EPICS)
 
@@ -8,7 +7,9 @@ ADRigaku UFXC area detector (EPICS)
 see: https://github.com/aps-8id-dys/ipython-8idiuser/issues/251
 """
 
-__all__ = ['adrigaku',]
+__all__ = [
+    "adrigaku",
+]
 
 from instrument.session_logs import logger
 from ophyd import ADComponent as ADCpt
@@ -30,18 +31,22 @@ class RigakuUfxcDetectorCam(CamBase):
 
     see: https://github.com/BCDA-APS/ADRigaku
     """
-    acquisition_delay = ADCpt(EpicsSignalWithRBV, 'AcquisitionDelay')
-    calibration_label = ADCpt(EpicsSignalWithRBV, 'CalibrationLabel', string=True)
-    exposure_delay = ADCpt(EpicsSignalWithRBV, 'ExposureDelay')
-    file_name = ADCpt(EpicsSignalWithRBV, 'FileName', string=True)
-    file_path = ADCpt(EpicsSignalWithRBV, 'FilePath', string=True)
-    lower_threshold = ADCpt(EpicsSignalWithRBV, 'LowerThreshold')
-    upper_threshold = ADCpt(EpicsSignalWithRBV, 'UpperThreshold')
+
+    acquisition_delay = ADCpt(EpicsSignalWithRBV, "AcquisitionDelay", kind="config")
+    calibration_label = ADCpt(EpicsSignalWithRBV, "CalibrationLabel", string=True, kind="config")
+    exposure_delay = ADCpt(EpicsSignalWithRBV, "ExposureDelay", kind="config")
+    file_name = ADCpt(EpicsSignalWithRBV, "FileName", string=True, kind="config")
+    file_path = ADCpt(EpicsSignalWithRBV, "FilePath", string=True, kind="config")
+    lower_threshold = ADCpt(EpicsSignalWithRBV, "LowerThreshold", kind="config")
+    upper_threshold = ADCpt(EpicsSignalWithRBV, "UpperThreshold", kind="config")
+
+    # remove these attributes from CamBase
+    pool_max_buffers = None
 
 
 class RigakuUfxcDetector(DetectorBase):
-    _html_docs = ['RigakuUfxcDoc.html']
-    cam = ADCpt(RigakuUfxcDetectorCam, 'cam1:')
+    _html_docs = ["RigakuUfxcDoc.html"]
+    cam = ADCpt(RigakuUfxcDetectorCam, "cam1:")
     # TODO: other plugins: Sparse0, IMM
 
 

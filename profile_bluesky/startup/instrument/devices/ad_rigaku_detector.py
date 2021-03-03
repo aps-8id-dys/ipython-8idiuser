ced0106c8d4bf9b241ecd6d39bfb706f4b0c8d29
Pete R Jemian <prjemian@gmail.com>
2021-03-03 11:55:01 -0600

    ENH #251 begin to add the staging work

--- profile_bluesky/startup/instrument/devices/ad_rigaku_detector.py
+++ profile_bluesky/startup/instrument/devices/ad_rigaku_detector.py
@@ -16,6 +16,7 @@ from ophyd import ADComponent as ADCpt
 from ophyd import EpicsSignal
 from ophyd import EpicsSignalRO
 from ophyd import EpicsSignalWithRBV
+from ophyd import Signal
 from ophyd.areadetector import CamBase
 from ophyd.areadetector import DetectorBase
 
@@ -40,6 +41,8 @@ class RigakuUfxcDetectorCam(CamBase):
     lower_threshold = ADCpt(EpicsSignalWithRBV, "LowerThreshold", kind="config")
     upper_threshold = ADCpt(EpicsSignalWithRBV, "UpperThreshold", kind="config")
 
+    corrections = ADCpt(EpicsSignal, "Corrections", kind="config", string=True)  # has no _RBV PV
+
     # remove these attributes from CamBase
     pool_max_buffers = None
 
@@ -48,6 +51,54 @@ class RigakuUfxcDetector(DetectorBase):
     _html_docs = ["RigakuUfxcDoc.html"]
     cam = ADCpt(RigakuUfxcDetectorCam, "cam1:")
     # TODO: other plugins: Sparse0, IMM
+
+    staging_mode = ADCpt(Signal, value="fast", kind="config")
+
+    def staging_setup_DM(self, *args, mode=None):
+
+        """
+        setup the detector's stage_sigs for acquisition with the DM workflow
+        from DM_DeviceMixinAreaDetector
+        """
+
+        # If staging stalls, it is because one or more of the signals
+        # is being set by its string value instead of the enumeration
+        # number.  This happens with EpicsSignalWithRBV when it was 
+        # called without the string=True kwarg.
+        #     In [13]: adrigaku.cam.image_mode.get()
+        #     Out[13]: 9
+
+        #     In [14]: adrigaku.cam.image_mode.get(as_string=True)
+        #     Out[14]: '16 Bit, 1S'
+        # The fix is to set by number, not string.
+        if (mode == 'fast'):
+            self.stage_sigs = {}
+            self.stage_sigs["cam.acquire_time"] = 20e-6
+            self.stage_sigs["cam.image_mode"] = 5
+            self.stage_sigs["cam.trigger_mode"] = 4
+            self.stage_sigs["cam.num_images"] = 100_000   # "_" is a visual separator
+            self.stage_sigs["cam.corrections"] = "Enabled"
+            self.stage_sigs["cam.data_type"] = "UInt32"
+            # TODO: what else is needed?
+
+        elif (mode == 'slow'):            self.stage_sigs = {}
+            self.stage_sigs["cam.image_mode"] = 9  # "16 Bit, 1S"
+            self.stage_sigs["cam.trigger_mode"] = 0  # "Fixed Time"
+            self.stage_sigs["cam.acquire_time"] = 0.1
+            self.stage_sigs["cam.num_images"] = 10
+            self.stage_sigs["cam.data_type"] = "UInt16"
+            self.stage_sigs["cam.corrections"] = "Disabled"
+            # TODO: what else is needed?
+
+            # TODO: Need the equivalent of the lines below:
+
+            # epics_put("8idRigaku:IMM1:AutoIncrement", "Yes")
+            # epics_put("8idRigaku:IMM1:NumCapture",10) 
+            # epics_put("8idRigaku:IMM1:FileNumber",1)
+            # epics_put("8idRigaku:IMM1:FilePath","/Rigaku/bin/destination/RigakuEpics/");
+            # epics_put("8idRigaku:IMM1:FileName","test");            
+
+        self.batch_name.put(self._file_name)
 
 
 adrigaku = RigakuUfxcDetector(IOC_PREFIX, name="adrigaku")

48554a1d98b7ed61a0e7dbbf89342618b984d3e2
qzhang234 <qzhang234@anl.gov>
2021-08-31 14:59:31 -0500

    qnw sample select enhancement

--- profile_bluesky/startup/instrument/plans/qnw_sample_select.py
+++ profile_bluesky/startup/instrument/plans/qnw_sample_select.py
@@ -3,55 +3,30 @@
 __all__ = [
     'select_sample',
     'te_qnw',
-    'qnw_params'
 ]
 
 import json
 from bluesky import plans as bp
 from bluesky import plan_stubs as bps
 from instrument.devices import samplestage, qnw_device
-from instrument.devices.qnw_device import qnw_env1, qnw_env2, qnw_env3
+from ..devices.qnw_device import qnw_env1, qnw_env2, qnw_env3
+from ..devices import aps
 from instrument.session_logs import logger
 logger.info(__file__)
 import os
 
+current_cycle = aps.aps_cycle.get()
+os.path.join('/home/beams/8IDIUSER/bluesky_data/', current_cycle[:4], current_cycle)
+qnw_file_path = os.path.join('/home/beams/8IDIUSER/bluesky_data/', current_cycle[:4], current_cycle)
 
-path = "/home/beams/8IDIUSER/bluesky_data/2021/2021-2"
-
-qnw_params = {}
 
 def select_sample(index=None):
     file_name = 'QNW_sample_definitions.json'
-    with open(os.path.join(path,file_name), 'r') as f1:
-        _ = json.load(f1)  
-
-    qnw_params = _[index]
-    return qnw_params
-    yield from bps.mv(samplestage.qnw_x, qnw_params['qnw_position'])  
-
-
-
-def te_qnw(set_temp=None, ramp_rate=5, temp_wait=False, tolerance=0.1):  
-
-    qnw_env = globals()[qnw_params["qnw_env"]]   
-    yield from bps.mv(qnw_env.ramprate, ramp_rate)
-
-    if temp_wait is True:
-        yield from bps.mv(qnw_env.tolerance, tolerance)
-        yield from bps.mv(qnw_env, set_temp)      
-    if temp_wait is False:
-        yield from bps.abs_set(qnw_env, set_temp)     
-
-
-
-"""
-def select_sample(index=None):
-    file_name = 'QNW_sample_definitions.json'
-    with open(os.path.join(path,file_name), 'r') as f1:
+    with open(os.path.join(qnw_file_path,file_name), 'r') as f1:
         _ = json.load(f1)  
 
     tmp = json.dumps(_[index], indent = 4)
-    with open(os.path.join(path,"current_sample.json"), 'w') as f2:
+    with open(os.path.join(qnw_file_path,"current_sample.json"), 'w') as f2:
         f2.write(tmp)
 
     yield from bps.mv(samplestage.qnw_x, _[index]['qnw_position'])  
@@ -60,9 +35,8 @@ def select_sample(index=None):
 def te_qnw(set_temp=None, ramp_rate=5, temp_wait=False, tolerance=0.1):  
 
     file_name = 'current_sample.json'
-    with open(os.path.join(path,file_name), 'r') as f1:
+    with open(os.path.join(qnw_file_path,file_name), 'r') as f1:
         _ = json.load(f1)
-    print(_["qnw_env"])
 
     if _["qnw_env"] == "qnw_env1":
         yield from bps.mv(qnw_env1.ramprate, ramp_rate)  
@@ -87,5 +61,32 @@ def te_qnw(set_temp=None, ramp_rate=5, temp_wait=False, tolerance=0.1):
             yield from bps.mv(qnw_env3, set_temp)      
         if temp_wait is False:
             yield from bps.abs_set(qnw_env3, set_temp)    
-"""
+
  
+
+"""
+
+global qnw_params
+qnw_params = {}
+
+def select_sample(index=None):
+    file_name = 'QNW_sample_definitions.json'
+    with open(os.path.join(path,file_name), 'r') as f1:
+        _ = json.load(f1)  
+
+    qnw_params = _[index]
+    print(qnw_params)
+    yield from bps.mv(samplestage.qnw_x, qnw_params['qnw_position'])  
+
+
+def te_qnw(set_temp=None, ramp_rate=5, temp_wait=False, tolerance=0.1):  
+
+    qnw_env = qnw_params["qnw_env"]  
+    yield from bps.mv(qnw_env.ramprate, ramp_rate)
+
+    if temp_wait is True:
+        yield from bps.mv(qnw_env.tolerance, tolerance)
+        yield from bps.mv(qnw_env, set_temp)      
+    if temp_wait is False:
+        yield from bps.abs_set(qnw_env, set_temp)     
+"""

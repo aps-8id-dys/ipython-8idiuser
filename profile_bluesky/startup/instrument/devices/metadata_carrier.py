fb13f601c2c754588a95f37d4c7e57af6f5ffa8b
Pete R Jemian <prjemian@users.noreply.github.com>
2022-02-16 16:29:38 -0600

    MNT #281 on review

--- profile_bluesky/startup/instrument/devices/metadata_carrier.py
+++ profile_bluesky/startup/instrument/devices/metadata_carrier.py
@@ -1,9 +1,10 @@
-
-
+"""
+Define common structures for configuration information.
+"""
 
 __all__ = [
-    'Init_User',
-    'Init_QNW',
+    'BasicUserInformation',
+    'QnwParameters',
 ]
 
 
@@ -13,7 +14,7 @@ from ophyd import Component as Cpt
 from bluesky import plan_stubs as bps
 
 
-class Init_User(Device): 
+class BasicUserInformation(Device):
     """
     Specify basic user information at beginning of the run,
     i.e. scan directories, data directory, q map
@@ -24,24 +25,26 @@ class Init_User(Device):
 
     def select_path(self, user_index):
         """
-        set user directories
+        bluesky plan: set user directories
         """
-        yield from bps.mv(self.det_directory, f'/home/8idiuser/{aps.aps_cycle.get()}/{user_index}')
-        yield from bps.mv(self.scan_directory, f'/home/beams10/8IDIUSER/bluesky_data/{aps.aps_cycle.get()}')
+        yield from bps.mv(
+            self.det_directory, f'/home/8idiuser/{aps.aps_cycle.get()}/{user_index}',
+            self.scan_directory, f'/home/beams10/8IDIUSER/bluesky_data/{aps.aps_cycle.get()}'
+        )
 
 
     def select_qmap(self, qmap_name: str):
         """
-        set qmap
+        bluesky plan: set qmap
         """
         yield from bps.mv(self.qmapname, f'/home/8-id-i/{aps.aps_cycle.get()}/{qmap_name}')
 
 
-class Init_QNW(Device):
+class QnwParameters(Device):
 
-    # TO-DO: Place holder. Need to merge this with QNW definition
+    # TODO: Place holder. Need to merge this with QNW definition
     # Make sure 'select' function reloads the json file containing sample info every time it's called
- 
+
     def __init__(self):
         self.sample_name = None
         self.id_char = None
@@ -53,7 +56,7 @@ class Init_QNW(Device):
         self.samz_num_points = None
         self.qnw_position = None
         self.qnw_name = None
-        
+
     def select(self, sample_index):
         """Load sample information from json file"""
         # read the json file
@@ -61,15 +64,3 @@ class Init_QNW(Device):
         config = json_dict[sample_index]
         self.sample_name = ["sample_name"]
         self.id_char = ["samp_id_char"]
-
-
-
-
-
-
-
-
-
-
-
-    

2f2c81d2be00ab111897a8b6c2673514d3862dcb
Gilberto Fabbris <gfabbris@anl.gov>
2022-05-29 15:01:41 -0500

    don't need to convert array to dask.

--- profile_bluesky/startup/instrument/framework/eiger_handler.py
+++ profile_bluesky/startup/instrument/framework/eiger_handler.py
@@ -4,7 +4,7 @@ Handler for the Eiger detector HDF5 files.
 
 # from area_detector_handlers.handlers import AreaDetectorHDF5SingleHandler
 from area_detector_handlers.handlers import AreaDetectorHDF5Handler
-from dask.array import from_array
+# from dask.array import from_array
 from ..session_logs import logger
 logger.info(__file__)
 
@@ -23,5 +23,5 @@ class EigerHDF5Handler(AreaDetectorHDF5Handler):
         {"AD_HDF5_Eiger500k_APS8ID"} | AreaDetectorHDF5Handler.specs
     )
 
-    def __call__(self, point_number):
-        return from_array(super().__call__(point_number))
+    # def __call__(self, point_number):
+    #     return from_array(super().__call__(point_number))
